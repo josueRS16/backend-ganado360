3f1ac6eb6f9c5e8531e4ae6503fd4ba0e1e83aee
josueRS16 <josue.serrano.rosales@est.una.ac.cr>
2026-08-11 22:56:05 -0600

    clean default values of .env

--- src/db/pool.js
+++ src/db/pool.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 
 // Crear pool de conexiones
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || 3306,
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '09082001',
-  database: process.env.MYSQL_DATABASE || 'ganadodb',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
